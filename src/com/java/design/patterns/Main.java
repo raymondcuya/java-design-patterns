e9bd505df3386347065422744587202605323b70
Raymond Cuya <raymondcuya@gmail.com>
2022-08-02 21:49:06 +1000

    calling mail service

--- src/com/java/design/patterns/Main.java
+++ src/com/java/design/patterns/Main.java
@@ -2,10 +2,8 @@ package com.java.design.patterns;
 
 public class Main {
     public static void main(String[] args) {
-        var account = new Account();
-        account.deposit(10);
-        account.withdraw(5);
-        System.out.println(account.getBalance());
+       var mailService = new MailService();
+       mailService.sendEmail();
     }
 
 }

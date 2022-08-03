d8ae523a21c4e1f59b1eaebca02d85b37ff92621
Raymond Cuya <raymondcuya@gmail.com>
2022-08-03 22:09:53 +1000

    inheritance

--- src/com/java/design/patterns/Main.java
+++ src/com/java/design/patterns/Main.java
@@ -2,8 +2,8 @@ package com.java.design.patterns;
 
 public class Main {
     public static void main(String[] args) {
-       var mailService = new MailService();
-       mailService.sendEmail();
+       var textBox = new TextBox();
+       textBox.enable();
     }
 
 }

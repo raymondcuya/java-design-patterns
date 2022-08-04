8ba50054dd361f3203362976cc5988f95c206163
Raymond Cuya <raymondcuya@gmail.com>
2022-08-04 21:23:16 +1000

    shows polymorphism in action

--- src/com/java/design/patterns/Main.java
+++ src/com/java/design/patterns/Main.java
@@ -2,8 +2,12 @@ package com.java.design.patterns;
 
 public class Main {
     public static void main(String[] args) {
-       var textBox = new TextBox();
-       textBox.enable();
+       drawUIControl(new TextBox());
+       drawUIControl(new CheckBox());
+    }
+
+    public static void drawUIControl(UIControl control) {
+        control.draw();
     }
 
 }

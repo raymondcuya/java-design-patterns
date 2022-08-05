481f04c6f4f5c8556e8a87a1968f26fa0cdde2b5
Raymond Cuya <raymondcuya@gmail.com>
2022-08-05 20:54:16 +1000

    editor sample class

---
+++ src/com/java/design/patterns/memento/Editor.java
@@ -0,0 +1,13 @@
+package com.java.design.patterns.memento;
+
+public class Editor {
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String content;
+}

f978d90d2ca7e69fbc57f31f28cff7724f439106
Raymond Cuya <raymondcuya@gmail.com>
2022-08-08 21:33:17 +1000

    restore and create state

--- src/com/java/design/patterns/memento/Editor.java
+++ src/com/java/design/patterns/memento/Editor.java
@@ -5,6 +5,14 @@ public class Editor {
         return content;
     }
 
+    public EditorState createState() {
+        return new EditorState(content);
+    }
+
+    public void restore(EditorState state) {
+        content = state.getContent();
+    }
+
     public void setContent(String content) {
         this.content = content;
     }

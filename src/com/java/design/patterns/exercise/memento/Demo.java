f42947f5c24d9ef2b8575ddf964c2ca17c05c8fc
Raymond Cuya <raymondcuya@gmail.com>
2022-08-16 21:06:18 +1000

    sample memento with other properties

--- src/com/java/design/patterns/exercise/memento/Demo.java
+++ src/com/java/design/patterns/exercise/memento/Demo.java
@@ -2,6 +2,23 @@ package com.java.design.patterns.exercise.memento;
 
 public class Demo {
     public static void main(String[] args) {
+        var document = new Document();
+        var history = new History();
 
+        document.setContent("Hello");
+        history.push(document.createMemento());
+
+        document.setFontName("Font 1");
+        history.push(document.createMemento());
+
+        document.setFontSize(10);
+
+        System.out.println(document);
+
+        document.restore(history.pop());
+        System.out.println(document);
+
+        document.restore(history.pop());
+        System.out.println(document);
     }
 }

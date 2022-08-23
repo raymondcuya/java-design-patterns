c8b3e1f608f99d4030e9b674c1de462317415a13
Raymond Cuya <raymondcuya@gmail.com>
2022-08-23 21:43:31 +1000

    sample of state pattern

---
+++ src/com/java/design/patterns/state/Main.java
@@ -0,0 +1,19 @@
+package com.java.design.patterns.state;
+
+public class Main {
+    public static void main(String[] args) {
+        var canvas = new Canvas();
+
+        canvas.setCurrentTool(new SelectionTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
+
+        canvas.setCurrentTool(new BrushTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
+
+        canvas.setCurrentTool(new EraserTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
+    }
+}

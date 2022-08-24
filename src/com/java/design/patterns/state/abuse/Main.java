84901e9696bd7575d91be9a8c137ca7035304361
Raymond Cuya <raymondcuya@gmail.com>
2022-08-24 21:29:49 +1000

    how stopwatch works

---
+++ src/com/java/design/patterns/state/abuse/Main.java
@@ -0,0 +1,10 @@
+package com.java.design.patterns.state.abuse;
+
+public class Main {
+    public static void main(String[] args) {
+        var stopwatch = new Stopwatch();
+        stopwatch.click();
+        stopwatch.click();
+        stopwatch.click();
+    }
+}

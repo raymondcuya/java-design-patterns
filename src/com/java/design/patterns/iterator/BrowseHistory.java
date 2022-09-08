ddc24f0409f5a1f687a41bff403d67adc13a7bd6
Raymond Cuya <raymondcuya@gmail.com>
2022-09-08 21:24:28 +1000

    a problem not using an iterator pattern

---
+++ src/com/java/design/patterns/iterator/BrowseHistory.java
@@ -0,0 +1,25 @@
+package com.java.design.patterns.iterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowseHistory {
+
+    private List<String> urls = new ArrayList<>();
+
+    public void push(String url) {
+        urls.add(url);
+    }
+
+    public String pop() {
+        var lastIndex = urls.size() - 1;
+        var lastUrl = urls.get(lastIndex);
+        urls.remove(lastUrl);
+
+        return lastUrl;
+    }
+
+    public List<String> getUrls() {
+        return urls;
+    }
+}

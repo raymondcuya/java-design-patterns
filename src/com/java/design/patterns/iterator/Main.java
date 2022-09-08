3a76a30203da8b7acf2e1385ba5e38a94ff27dc0
Raymond Cuya <raymondcuya@gmail.com>
2022-09-08 21:25:59 +1000

    main call is too dependent on the histories implementation

---
+++ src/com/java/design/patterns/iterator/Main.java
@@ -0,0 +1,15 @@
+package com.java.design.patterns.iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        var history = new BrowseHistory();
+        history.push("a");
+        history.push("b");
+        history.push("c");
+
+        for (var i = 0; i < history.getUrls().size(); i++) {
+            var url = history.getUrls().get(i);
+            System.out.println(url);
+        }
+    }
+}

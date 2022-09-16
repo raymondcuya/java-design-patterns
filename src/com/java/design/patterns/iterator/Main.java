1496a2029f4d491a3d839b31ac00ca6a0d976887
Raymond Cuya <raymondcuya@gmail.com>
2022-09-16 23:01:06 +1000

    sample implementation of history and iterator pattern

--- src/com/java/design/patterns/iterator/Main.java
+++ src/com/java/design/patterns/iterator/Main.java
@@ -2,12 +2,28 @@ package com.java.design.patterns.iterator;
 
 public class Main {
     public static void main(String[] args) {
-        var history = new BrowseHistory();
+        var history = (History) new BrowseHistory();
         history.push("a");
         history.push("b");
         history.push("c");
+        history.push("d");
+        history.pop();
 
         Iterator iterator = history.createIterator();
+        while(iterator.hasNext()) {
+            var url = iterator.current();
+            System.out.println(url);
+            iterator.next();
+        }
+
+        history = (History) new BrowseHistoryArray();
+        history.push("a");
+        history.push("b");
+        history.push("c");
+        history.push("d");
+        history.pop();
+
+        iterator = history.createIterator();
         while(iterator.hasNext()) {
             var url = iterator.current();
             System.out.println(url);

0ebbfa07865a3848216c8caa205aeeab9c63d8e3
Raymond Cuya <raymondcuya@gmail.com>
2022-09-22 21:06:44 +1000

    implementation to add products

--- src/com/java/design/patterns/iterator/exercise/ProductCollection.java
+++ src/com/java/design/patterns/iterator/exercise/ProductCollection.java
@@ -1,4 +1,12 @@
 package com.java.design.patterns.iterator.exercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductCollection {
+    private List<Product> products = new ArrayList<>();
+
+    public void add(Product product) {
+        products.add(product);
+    }
 }

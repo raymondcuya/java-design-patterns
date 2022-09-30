937b2963f333db353fc21093cdb0d1aff966bc42
Raymond Cuya <raymondcuya@gmail.com>
2022-09-30 22:44:32 +1000

    strategy pattern for compressor and filter

--- src/com/java/design/patterns/strategy/ImageStorage.java
+++ src/com/java/design/patterns/strategy/ImageStorage.java
@@ -1,24 +1,17 @@
 package com.java.design.patterns.strategy;
 
 public class ImageStorage {
-    private String compressor;
-    private String filter;
+    private Compressor compressor;
+    private Filter filter;
 
-    public ImageStorage(String compressor, String filter) {
+    public ImageStorage(Compressor compressor, Filter filter) {
         this.compressor = compressor;
         this.filter = filter;
     }
 
     public void store(String filename) {
-        if (compressor == "jpeg")
-            System.out.println("Compressing using JPEG");
-        else if (compressor == "png")
-            System.out.println("Compressing using PNG");
+        compressor.compress(filename);
+        filter.apply(filename);
 
-
-        if (filter == "b&w")
-            System.out.println("Applying B&W filter");
-        else if (filter == "high-contrast")
-            System.out.println("Applying high contrast filter");
     }
 }

7eaf5613d39cbc5d08e4d24dcbea44f99f67bd53
Raymond Cuya <raymondcuya@gmail.com>
2022-09-30 22:43:42 +1000

    filter implementation for b&w

---
+++ src/com/java/design/patterns/strategy/BlackAndWhiteFilter.java
@@ -0,0 +1,8 @@
+package com.java.design.patterns.strategy;
+
+public class BlackAndWhiteFilter implements Filter {
+    @Override
+    public void apply(String filename) {
+        System.out.println("Applying B&W filter");
+    }
+}

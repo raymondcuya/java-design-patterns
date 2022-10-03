ab0914ceb6d68ae091ca6a9dafc6fa58c3174af2
Raymond Cuya <raymondcuya@gmail.com>
2022-10-03 20:41:51 +1100

    removed constuctor

--- src/com/java/design/patterns/strategy/ImageStorage.java
+++ src/com/java/design/patterns/strategy/ImageStorage.java
@@ -1,17 +1,8 @@
 package com.java.design.patterns.strategy;
 
 public class ImageStorage {
-    private Compressor compressor;
-    private Filter filter;
-
-    public ImageStorage(Compressor compressor, Filter filter) {
-        this.compressor = compressor;
-        this.filter = filter;
-    }
-
-    public void store(String filename) {
+    public void store(String filename, Compressor compressor, Filter filter) {
         compressor.compress(filename);
         filter.apply(filename);
-
     }
 }

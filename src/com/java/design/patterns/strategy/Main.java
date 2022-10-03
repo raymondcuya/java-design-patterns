dc3be27f99303fd598af923050fb724b53e9b3b4
Raymond Cuya <raymondcuya@gmail.com>
2022-10-03 20:42:24 +1100

    instantiate compressor and filter on store method

--- src/com/java/design/patterns/strategy/Main.java
+++ src/com/java/design/patterns/strategy/Main.java
@@ -2,7 +2,8 @@ package com.java.design.patterns.strategy;
 
 public class Main {
     public static void main(String[] args) {
-        var imageStorage = new ImageStorage(new JpegCompressor(), new BlackAndWhiteFilter());
-        imageStorage.store("a");
+        var imageStorage = new ImageStorage();
+        imageStorage.store("a", new JpegCompressor(), new BlackAndWhiteFilter());
+        imageStorage.store("b", new PngCompressor(), new BlackAndWhiteFilter());
     }
 }

4b719387e32e9507b001f3298cdb9a7da5f6dde8
Raymond Cuya <raymondcuya@gmail.com>
2022-10-07 22:35:28 +1100

    AES implementation

--- src/com/java/design/patterns/strategy/exercise/AES.java
+++ src/com/java/design/patterns/strategy/exercise/AES.java
@@ -1,4 +1,9 @@
 package com.java.design.patterns.strategy.exercise;
 
-public class AES {
+public class AES implements EncryptedAlgorithm {
+    @Override
+    public String encrypt(String message) {
+        System.out.println("AES Encryption");
+        return "AES Encypted Message";
+    }
 }

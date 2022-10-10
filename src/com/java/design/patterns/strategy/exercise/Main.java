0b70b279bd789ca6e7a10a6f0beef5c881c45d89
Raymond Cuya <raymondcuya@gmail.com>
2022-10-10 21:34:41 +1100

    remove the constructor and use the method instead

--- src/com/java/design/patterns/strategy/exercise/Main.java
+++ src/com/java/design/patterns/strategy/exercise/Main.java
@@ -3,10 +3,8 @@ package com.java.design.patterns.strategy.exercise;
 public class Main {
 
     public static void main(String[] args) {
-        var chatClient = new ChatClient(new AES());
-        chatClient.send("Hello world");
-
-        chatClient = new ChatClient(new DES());
-        chatClient.send("Hello world");
+        var chatClient = new ChatClient();
+        chatClient.send("Hello world", new AES());
+        chatClient.send("Hello world", new DES());
     }
 }

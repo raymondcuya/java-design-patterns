78f6656217fadcc5dc31eaef5a7343cf163dee65
Raymond Cuya <raymondcuya@gmail.com>
2022-10-11 21:54:17 +1100

    task to transfer money

---
+++ src/com/java/design/patterns/template/TransferMoneyTask.java
@@ -0,0 +1,14 @@
+package com.java.design.patterns.template;
+
+public class TransferMoneyTask {
+    private AuditTrail auditTrail;
+
+    public TransferMoneyTask(AuditTrail auditTrail) {
+        this.auditTrail = auditTrail;
+    }
+
+    public void execute() {
+        auditTrail.record();
+        System.out.println("Transfer Money");
+    }
+}

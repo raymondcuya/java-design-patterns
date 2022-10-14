1ddb543abc3fdc35adac9e445e5822573e630349
Raymond Cuya <raymondcuya@gmail.com>
2022-10-14 21:55:25 +1100

    transfer money tasks using template pattern

---
+++ src/com/java/design/patterns/template/pattern/TransferMoneyTask.java
@@ -0,0 +1,8 @@
+package com.java.design.patterns.template.pattern;
+
+public class TransferMoneyTask extends Task{
+    @Override
+    protected void doExecute() {
+        System.out.println("Transfer Money");
+    }
+}

f2abf71b4a65f39fcead5c209956d5ece07253d1
Raymond Cuya <raymondcuya@gmail.com>
2022-10-17 21:05:47 +1100

    problemm to solve

--- src/com/java/design/patterns/template/exercise/Window.java
+++ src/com/java/design/patterns/template/exercise/Window.java
@@ -1,4 +1,13 @@
 package com.java.design.patterns.template.exercise;
 
 public class Window {
+    public void close() {
+        //TODO: custom windows may need to execute some code before the window
+        // is closed.
+
+        System.out.println("Removing the window from the screen");
+
+        //TODO: custom windows may need to execute some code after the window
+        // is closed.
+    }
 }

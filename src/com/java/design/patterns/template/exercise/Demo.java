08303c171b4d13e4f3f29842bb45cfd9d4f33949
Raymond Cuya <raymondcuya@gmail.com>
2022-10-19 22:38:12 +1100

    solution for calling windows using template pattern

---
+++ src/com/java/design/patterns/template/exercise/Demo.java
@@ -0,0 +1,11 @@
+package com.java.design.patterns.template.exercise;
+
+import com.java.design.patterns.template.exercise.template.ChatWindow;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        var window = new ChatWindow();
+        window.close();
+    }
+}

5262d1b54f92a8ed4283675fc7e2d14f56cfbe9c
Raymond Cuya <raymondcuya@gmail.com>
2022-10-24 20:40:05 +1100

    calling the command interface inside the button

--- src/com/java/design/patterns/command/Button.java
+++ src/com/java/design/patterns/command/Button.java
@@ -3,9 +3,14 @@ package com.java.design.patterns.command;
 public class Button {
 
     private String label;
+    private Command command;
+
+    public Button(Command command) {
+        this.command = command;
+    }
 
     public void click() {
-        // ...
+        command.execute();
     }
 
     public String getLabel() {

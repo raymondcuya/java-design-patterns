9f253c3e7ff32909d8ee492246440a47ca7e0e61
Raymond Cuya <raymondcuya@gmail.com>
2022-10-24 20:33:58 +1100

    button

---
+++ src/com/java/design/patterns/command/Button.java
@@ -0,0 +1,18 @@
+package com.java.design.patterns.command;
+
+public class Button {
+
+    private String label;
+
+    public void click() {
+        // ...
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}

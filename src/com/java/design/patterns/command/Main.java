366f194910b23b86241e041d8561dc2956225f46
Raymond Cuya <raymondcuya@gmail.com>
2022-10-28 22:33:55 +1100

    sample call of command pattern

---
+++ src/com/java/design/patterns/command/Main.java
@@ -0,0 +1,12 @@
+package com.java.design.patterns.command;
+
+import com.java.design.patterns.command.fx.Button;
+
+public class Main {
+    public static void main(String[] args) {
+        var service = new CustomerService();
+        var command = new AddCustomerCommand(service);
+        var button = new Button(command);
+        button.click();
+    }
+}

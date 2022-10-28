35847789d7b5909f19022f00e42b8e5fdbe68cb3
Raymond Cuya <raymondcuya@gmail.com>
2022-10-28 22:32:54 +1100

    sample command pattern

---
+++ src/com/java/design/patterns/command/AddCustomerCommand.java
@@ -0,0 +1,16 @@
+package com.java.design.patterns.command;
+
+import com.java.design.patterns.command.fx.Command;
+
+public class AddCustomerCommand implements Command {
+    private CustomerService service;
+
+    public AddCustomerCommand(CustomerService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void execute() {
+        service.addCustomer();
+    }
+}

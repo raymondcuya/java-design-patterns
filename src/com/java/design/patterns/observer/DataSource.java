7c31c5dbbed3172c67fedc65eb4ec7f8d964cc67
Raymond Cuya <raymondcuya@gmail.com>
2022-11-21 20:13:16 +1100

    datasource for observer pattern

---
+++ src/com/java/design/patterns/observer/DataSource.java
@@ -0,0 +1,13 @@
+package com.java.design.patterns.observer;
+
+public class DataSource {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}

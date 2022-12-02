79ab654d2722e69cfd08f39825b39631638ee65c
Raymond Cuya <raymondcuya@gmail.com>
2022-12-03 00:00:52 +1100

    implements spreadsheet for observer pattern

---
+++ src/com/java/design/patterns/observer/pull/Spreadsheet.java
@@ -0,0 +1,12 @@
+package com.java.design.patterns.observer.pull;
+public class Spreadsheet implements Observer {
+    private DataSource dataSource;
+
+    public Spreadsheet(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Override
+    public void update() {
+        System.out.println("Spreadsheet got notified: " + dataSource.getValue());
+    }
+}

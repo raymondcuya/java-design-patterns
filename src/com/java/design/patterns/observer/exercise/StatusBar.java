7844b8802ba2703423750b2ad5f00ec24314f979
Raymond Cuya <raymondcuya@gmail.com>
2022-12-09 22:47:25 +1100

    implementing an observer pattern

--- src/com/java/design/patterns/observer/exercise/StatusBar.java
+++ src/com/java/design/patterns/observer/exercise/StatusBar.java
@@ -3,16 +3,24 @@ package com.java.design.patterns.observer.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StatusBar {
+public class StatusBar implements Observer {
     private List<Stock> stocks = new ArrayList<>();
 
     public void addStock(Stock stock) {
         stocks.add(stock);
+        stock.addObserver(this);
     }
 
     public void show() {
+        System.out.println("Status Bar");
         for (var stock : stocks)
             System.out.println(stock);
+    }
+
+    @Override
+    public void priceChanged() {
+        System.out.println("Price Changed - Refreshing StatusBar");
+        show();
     }
 }
 

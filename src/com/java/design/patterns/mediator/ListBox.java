64606ede12d3f74f570af026200b0d4911534b38
Raymond Cuya <raymondcuya@gmail.com>
2022-12-14 20:51:25 +1100

    listbox selection

---
+++ src/com/java/design/patterns/mediator/ListBox.java
@@ -0,0 +1,14 @@
+package com.java.design.patterns.mediator;
+
+public class ListBox extends UIControl {
+
+    private String selection;
+
+    public String getSelection() {
+        return selection;
+    }
+
+    public void setSelection(String selection) {
+        this.selection = selection;
+    }
+}

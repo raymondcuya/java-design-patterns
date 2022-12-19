0a9737ba4b88f408b5f07e27c71368f47fdcd93b
Raymond Cuya <raymondcuya@gmail.com>
2022-12-19 20:50:27 +1100

    main control

--- src/com/java/design/patterns/mediator/UIControl.java
+++ src/com/java/design/patterns/mediator/UIControl.java
@@ -1,4 +1,9 @@
 package com.java.design.patterns.mediator;
 
 public class UIControl {
+    public UIControl(DialogBox owner) {
+        this.owner = owner;
+    }
+
+    protected DialogBox owner;
 }

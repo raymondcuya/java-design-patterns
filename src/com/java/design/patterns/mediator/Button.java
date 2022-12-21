8a813ec73d6c30441f5da32ff45abefe94612ee6
Raymond Cuya <raymondcuya@gmail.com>
2022-12-21 21:43:03 +1100

    dialogbox as constructor

--- src/com/java/design/patterns/mediator/Button.java
+++ src/com/java/design/patterns/mediator/Button.java
@@ -1,6 +1,10 @@
 package com.java.design.patterns.mediator;
 
 public class Button extends UIControl {
+    public Button(DialogBox owner) {
+        super(owner);
+    }
+
     private boolean isEnabled;
 
     public boolean isEnabled() {
@@ -9,5 +13,6 @@ public class Button extends UIControl {
 
     public void setEnabled(boolean enabled) {
         isEnabled = enabled;
+        owner.changed(this);
     }
 }

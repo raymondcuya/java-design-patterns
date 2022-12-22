8cf86e2eb88b9515724b38a9ec16c8b737cd4ae7
Raymond Cuya <raymondcuya@gmail.com>
2022-12-22 22:13:01 +1100

    mediator class

---
+++ src/com/java/design/patterns/mediator/ArticlesDialogBox.java
@@ -0,0 +1,34 @@
+package com.java.design.patterns.mediator;
+
+public class ArticlesDialogBox extends DialogBox {
+    private ListBox articlesListBox = new ListBox(this);
+    private TextBox titleTextBox = new TextBox(this);
+    private Button saveButton = new Button(this);
+
+    public void simulateUserInteraction() {
+        articlesListBox.setSelection("Article 1");
+        titleTextBox.setContent("");
+        articlesListBox.setSelection("Article 2");
+        System.out.println("TextBox: " + titleTextBox.getContent());
+        System.out.println("Button: " + saveButton.isEnabled());
+    }
+    @Override
+    void changed(UIControl control) {
+        if (control == articlesListBox)
+            articleSelect();
+        else if (control == titleTextBox) {
+            titleChanged();
+        }
+    }
+
+    private void titleChanged() {
+        var content = titleTextBox.getContent();
+        var isEmpty = (content == null || content.isEmpty());
+        saveButton.setEnabled(!isEmpty);
+    }
+
+    private void articleSelect() {
+        titleTextBox.setContent(articlesListBox.getSelection());
+        saveButton.setEnabled(true);
+    }
+}

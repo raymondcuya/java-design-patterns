5207ff4eeb847a9d2596139e281591a744dfaaa0
Raymond Cuya <raymondcuya@gmail.com>
2023-01-05 22:57:23 +1100

    listbox with selection

---
+++ src/com/java/design/patterns/mediator/observer/ListBox.java
@@ -0,0 +1,12 @@
+package com.java.design.patterns.mediator.observer;
+
+public class ListBox extends UIControl {
+    private String selection;
+    public String getSelection() {
+        return selection;
+    }
+    public void setSelection(String selection) {
+        this.selection = selection;
+        notifyEventHandlers();
+    }
+}

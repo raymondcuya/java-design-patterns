3225706ce632f5da3a5dccdd27f2c45c339e7b20
Raymond Cuya <raymondcuya@gmail.com>
2023-01-05 22:57:42 +1100

    textbox with content

---
+++ src/com/java/design/patterns/mediator/observer/TextBox.java
@@ -0,0 +1,11 @@
+package com.java.design.patterns.mediator.observer;
+public class TextBox extends UIControl {
+    private String content;
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+        notifyEventHandlers();
+    }
+}

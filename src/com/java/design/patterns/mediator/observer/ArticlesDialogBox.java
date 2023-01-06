b3410469fe44594e957a6cc0eeb945e70e347579
Raymond Cuya <raymondcuya@gmail.com>
2023-01-06 23:30:36 +1100

    observer with mediator pattern

---
+++ src/com/java/design/patterns/mediator/observer/ArticlesDialogBox.java
@@ -0,0 +1,28 @@
+package com.java.design.patterns.mediator.observer;
+public class ArticlesDialogBox {
+    private ListBox articlesListBox = new ListBox();
+    private TextBox titleTextBox = new TextBox();
+    private Button saveButton = new Button();
+
+    public ArticlesDialogBox() {
+        articlesListBox.addEventHandler(this::articleSelected);
+        titleTextBox.addEventHandler(this::titleChanged);
+    }
+
+    public void simulateUserInteraction() {
+        articlesListBox.setSelection("Article 1");
+        titleTextBox.setContent("");
+        articlesListBox.setSelection("Article 2");
+        System.out.println("TextBox: " + titleTextBox.getContent());
+        System.out.println("Button: " + saveButton.isEnabled());
+    }
+    private void titleChanged() {
+        var content = titleTextBox.getContent();
+        var isEmpty = (content == null || content.isEmpty());
+        saveButton.setEnabled(!isEmpty);
+    }
+    private void articleSelected() {
+        titleTextBox.setContent(articlesListBox.getSelection());
+        saveButton.setEnabled(true);
+    }
+}

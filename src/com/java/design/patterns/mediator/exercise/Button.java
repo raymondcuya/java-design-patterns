3d80a3fc36b367352019e6513269d0e5786b4313
Raymond Cuya <raymondcuya@gmail.com>
2023-01-10 23:01:27 +1100

    mediator exercise files

---
+++ src/com/java/design/patterns/mediator/exercise/Button.java
@@ -0,0 +1,13 @@
+package com.java.design.patterns.mediator.exercise;
+
+public class Button extends UIControl {
+    private boolean isEnabled;
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
+}
---
+++ src/com/java/design/patterns/mediator/exercise/CheckBox.java
@@ -0,0 +1,13 @@
+package com.java.design.patterns.mediator.exercise;
+
+public class CheckBox extends UIControl {
+    private boolean isChecked;
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+}

27a82c7b9340170ef844674ee0ac4617c876a12f
Raymond Cuya <raymondcuya@gmail.com>
2023-01-11 21:34:28 +1100

    observable control

--- src/com/java/design/patterns/mediator/exercise/UIControl.java
+++ src/com/java/design/patterns/mediator/exercise/UIControl.java
@@ -1,4 +1,17 @@
 package com.java.design.patterns.mediator.exercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UIControl {
+    private List<EventHandler> eventHandlers = new ArrayList<>();
+
+    public void addEventHandler(EventHandler handler) {
+        eventHandlers.add(handler);
+    }
+
+    public void notifyEventHandlers() {
+        for (var handler : eventHandlers)
+            handler.handle();
+    }
 }

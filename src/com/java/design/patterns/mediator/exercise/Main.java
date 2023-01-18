e811edfa46965115f95c4877cf2360b01d974ca7
Raymond Cuya <raymondcuya@gmail.com>
2023-01-18 21:25:50 +1100

    call to simulate interaction

---
+++ src/com/java/design/patterns/mediator/exercise/Main.java
@@ -0,0 +1,8 @@
+package com.java.design.patterns.mediator.exercise;
+
+public class Main {
+    public static void main(String[] args) {
+        var signUpDialogBox = new SignUpDialogBox();
+        signUpDialogBox.simulateUserInteraction();
+    }
+}

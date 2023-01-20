81ab06d1a4378e6f465ac8bf19826518b28a5277
Raymond Cuya <raymondcuya@gmail.com>
2023-01-20 23:49:12 +1100

    compress request

---
+++ src/com/java/design/patterns/chainOfResponsibility/Compressor.java
@@ -0,0 +1,7 @@
+package com.java.design.patterns.chainOfResponsibility;
+
+public class Compressor {
+    public void compress(HttpRequest request) {
+        System.out.println("Compress");
+    }
+}

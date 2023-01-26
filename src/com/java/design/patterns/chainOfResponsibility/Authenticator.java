cb101f87c450ce38bf795083a2899fe7b54157e8
Raymond Cuya <raymondcuya@gmail.com>
2023-01-26 22:45:58 +1100

    authentication handler

--- src/com/java/design/patterns/chainOfResponsibility/Authenticator.java
+++ src/com/java/design/patterns/chainOfResponsibility/Authenticator.java
@@ -1,10 +1,15 @@
 package com.java.design.patterns.chainOfResponsibility;
 
-public class Authenticator {
-    public boolean authenticate(HttpRequest request) {
+public class Authenticator extends Handler {
+    public Authenticator(Handler next) {
+        super(next);
+    }
+
+    @Override
+    public boolean doHandle(HttpRequest request) {
         var isValid = (request.getUsername() == "admin"
                 && request.getPassword() == "1234");
         System.out.println("Authentication");
-        return isValid;
+        return !isValid;
     }
 }

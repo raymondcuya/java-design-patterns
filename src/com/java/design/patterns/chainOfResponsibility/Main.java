35b53044d78ab5a80ac5985ef520e542f685b91c
Raymond Cuya <raymondcuya@gmail.com>
2023-02-01 19:27:41 +1100

    sample call of chain of responsibility

---
+++ src/com/java/design/patterns/chainOfResponsibility/Main.java
@@ -0,0 +1,11 @@
+package com.java.design.patterns.chainOfResponsibility;
+
+public class Main {
+    public static void main(String[] args) {
+        var compressor = new Compressor(null);
+        var logger = new Logger(compressor);
+        var authenticator = new Authenticator(logger);
+        var server = new WebServer(authenticator);
+        server.handle(new HttpRequest("admin", "1234"));
+    }
+}

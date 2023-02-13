19ca6e7b535ebb1b3c595072fbaaaf38ebaf5faa
Raymond Cuya <raymondcuya@gmail.com>
2023-02-13 20:41:53 +1100

    main visitor

---
+++ src/com/java/design/patterns/visitor/Main.java
@@ -0,0 +1,10 @@
+package com.java.design.patterns.visitor;
+
+public class Main {
+    public static void main(String[] args) {
+        var document = new HtmlDocument();
+        document.add(new HeadingNode());
+        document.add(new AnchorNode());
+        document.hightlight();
+    }
+}

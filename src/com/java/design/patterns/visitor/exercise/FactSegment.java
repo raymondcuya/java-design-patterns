19c1027dade5558f0206d5ad58122b793c804779
Raymond Cuya <raymondcuya@gmail.com>
2023-03-03 22:26:49 +1100

    applying filter

--- src/com/java/design/patterns/visitor/exercise/FactSegment.java
+++ src/com/java/design/patterns/visitor/exercise/FactSegment.java
@@ -1,4 +1,8 @@
 package com.java.design.patterns.visitor.exercise;
 
 public class FactSegment extends Segment {
+  @Override
+  public void applyFilter(AudioFilter filter) {
+    filter.apply(this);
+  }
 }
--- src/com/java/design/patterns/visitor/exercise/FormatSegment.java
+++ src/com/java/design/patterns/visitor/exercise/FormatSegment.java
@@ -1,4 +1,8 @@
 package com.java.design.patterns.visitor.exercise;
 
 public class FormatSegment extends Segment {
+  @Override
+  public void applyFilter(AudioFilter filter) {
+    filter.apply(this);
+  }
 }

a6bb36b14e391e61a401024044586b375fb0986c
Raymond Cuya <raymondcuya@gmail.com>
2023-03-22 22:35:02 +1100

    calling team to deploy

---
+++ src/com/java/design/patterns/composite/exercise/Main.java
@@ -0,0 +1,21 @@
+package com.java.design.patterns.composite.exercise;
+
+public class Main {
+    public static void main(String[] args) {
+        var subTeam1 = new Team();
+        subTeam1.add(new Truck());
+        subTeam1.add(new HumanResource());
+        subTeam1.add(new HumanResource());
+
+        var subTeam2 = new Team();
+        subTeam2.add(new Truck());
+        subTeam2.add(new HumanResource());
+        subTeam2.add(new HumanResource());
+
+        var team = new Team();
+        team.add(subTeam1);
+        team.add(subTeam2);
+
+        team.deploy();
+    }
+}

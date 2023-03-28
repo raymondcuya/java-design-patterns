cc7fe69f20f194577c3794fae9e2a594b8596340
Raymond Cuya <raymondcuya@gmail.com>
2023-03-28 19:19:34 +1100

    implement resource

--- src/com/java/design/patterns/composite/exercise/Truck.java
+++ src/com/java/design/patterns/composite/exercise/Truck.java
@@ -1,6 +1,6 @@
 package com.java.design.patterns.composite.exercise;
 
-public class Truck {
+public class Truck extends Resource {
   public void deploy() {
     System.out.println("Deploying a truck");
   }

7a93f4580f207f7da8803124f50dee238bf085d9
Raymond Cuya <raymondcuya@gmail.com>
2023-03-28 19:19:06 +1100

    implement resource

--- src/com/java/design/patterns/composite/exercise/HumanResource.java
+++ src/com/java/design/patterns/composite/exercise/HumanResource.java
@@ -1,6 +1,6 @@
 package com.java.design.patterns.composite.exercise;
 
-public class HumanResource {
+public class HumanResource extends Resource {
   public void deploy() {
     System.out.println("Deploying a human resource");
   }

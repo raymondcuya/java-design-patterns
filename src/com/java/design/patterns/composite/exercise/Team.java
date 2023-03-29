4d77d9f650587afd06d2678ee1a73cd1d2116b9c
raymond cuya <raymondcuya@gmail.com>
2023-03-30 00:16:59 +1100

    make resources generic

--- src/com/java/design/patterns/composite/exercise/Team.java
+++ src/com/java/design/patterns/composite/exercise/Team.java
@@ -3,21 +3,16 @@ package com.java.design.patterns.composite.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team {
-  private List<Object> resources = new ArrayList<>();
+public class Team extends Resource {
+  private final List<Resource> resources = new ArrayList<>();
 
-  public void add(Object resource) {
+  public void add(Resource resource) {
     resources.add(resource);
   }
 
   public void deploy() {
-    for (var resource : resources) {
-      if (resource instanceof Truck)
-        ((Truck)resource).deploy();
-      else if (resource instanceof HumanResource)
-        ((HumanResource)resource).deploy();
-      else
-        ((Team)resource).deploy();
-    }
+    for (var resource : resources)
+      resource.deploy();
   }
 }
+

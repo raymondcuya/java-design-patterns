893d46d76e28f6768035644af9c9b8a3d144dd68
raymond cuya <raymondcuya@gmail.com>
2023-04-05 22:33:17 +1000

    caramel filter

---
+++ src/com/java/design/patterns/adapter/avaFilters/Caramel.java
@@ -0,0 +1,13 @@
+package com.java.design.patterns.adapter.avaFilters;
+
+import com.java.design.patterns.adapter.Image;
+
+public class Caramel {
+    public void init() {
+
+    }
+
+    public void render(Image image) {
+        System.out.println("Applying Caramel Filter");
+    }
+}

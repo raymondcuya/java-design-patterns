1a17c959e4fed0a0dcb11c23e67d000af4f3a644
raymond cuya <raymondcuya@gmail.com>
2023-04-07 23:39:25 +1000

    using Caramel Filter composition adapter

--- src/com/java/design/patterns/adapter/Main.java
+++ src/com/java/design/patterns/adapter/Main.java
@@ -1,8 +1,10 @@
 package com.java.design.patterns.adapter;
 
+import com.java.design.patterns.adapter.avaFilters.Caramel;
+
 public class Main {
     public static void main(String[] args) {
         var imageView = new ImageView(new Image());
-        imageView.apply(new VividFilter());
+        imageView.apply(new CaramelFilter(new Caramel()));
     }
 }

0cbec6985bd9c77e1a72450d279f4a18076c699b
raymond cuya <raymondcuya@gmail.com>
2023-04-07 23:38:45 +1000

    adapter pattern by composition

---
+++ src/com/java/design/patterns/adapter/CaramelFilter.java
@@ -0,0 +1,16 @@
+package com.java.design.patterns.adapter;
+
+import com.java.design.patterns.adapter.avaFilters.Caramel;
+
+public class CaramelFilter implements Filter {
+    private Caramel caramel;
+    public CaramelFilter(Caramel caramel) {
+        this.caramel = caramel;
+    }
+
+    @Override
+    public void apply(Image image) {
+        caramel.init();
+        caramel.render(image);
+    }
+}

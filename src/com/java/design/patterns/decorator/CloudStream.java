c7696d1a06ae3eacdc0f6d83dc87871b98899637
raymond cuya <raymondcuya@gmail.com>
2023-05-11 21:08:57 +1000

    cloud implementation of stream

--- src/com/java/design/patterns/decorator/CloudStream.java
+++ src/com/java/design/patterns/decorator/CloudStream.java
@@ -1,6 +1,6 @@
 package com.java.design.patterns.decorator;
 
-public class CloudStream {
+public class CloudStream implements Stream {
     public void write(String data) {
         System.out.println("Storing " + data);
     }

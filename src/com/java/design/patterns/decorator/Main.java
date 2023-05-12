d0bf223241a32fa780234f7f4fa2e76794c305e4
raymond cuya <raymondcuya@gmail.com>
2023-05-12 23:37:13 +1000

    sample decorator pattern

--- src/com/java/design/patterns/decorator/Main.java
+++ src/com/java/design/patterns/decorator/Main.java
@@ -2,7 +2,11 @@ package com.java.design.patterns.decorator;
 
 public class Main {
     public static void main(String[] args) {
-        var cloudStream = new EncryptedCloudStream();
-        cloudStream.write("Here's my data");
+        storeCreditCard(new EncryptedCloudStream(new CompressedCloudStream(new CloudStream())));
+    }
+
+    public static void storeCreditCard(Stream stream) {
+        stream.write("1234-1234-1234-1234");
+
     }
 }

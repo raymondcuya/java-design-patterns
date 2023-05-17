b4bcee8d5ad41e2d686f0761d33485b047fc74ea
raymond cuya <raymondcuya@gmail.com>
2023-05-17 20:52:16 +1000

    calling editor

---
+++ src/com/java/design/patterns/decorator/exercise/Main.java
@@ -0,0 +1,8 @@
+package com.java.design.patterns.decorator.exercise;
+
+public class Main {
+    public static void main(String[] args) {
+        var editor = new Editor();
+        editor.openProject("...");
+    }
+}

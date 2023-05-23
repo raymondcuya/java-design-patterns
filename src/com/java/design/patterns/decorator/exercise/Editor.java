485363b552832c02ca603612dc9a39d167987e34
raymond cuya <raymondcuya@gmail.com>
2023-05-23 22:08:01 +1000

    called the main and error decorator

--- src/com/java/design/patterns/decorator/exercise/Editor.java
+++ src/com/java/design/patterns/decorator/exercise/Editor.java
@@ -2,15 +2,19 @@ package com.java.design.patterns.decorator.exercise;
 
 public class Editor {
     public void openProject(String path) {
-        Artefact[] artefacts = {
+        AbstractArtefact[] artefacts = {
                 new Artefact("Main"),
                 new Artefact("Demo"),
                 new Artefact("EmailClient"),
                 new Artefact("EmailProvider"),
         };
 
-        artefacts[0].setMain(true);
-        artefacts[2].setHasError(true);
+        // We can apply multiple decorators
+        artefacts[0] = new ErrorDecorator(new MainDecorator(artefacts[0]));
+        artefacts[2] = new ErrorDecorator(artefacts[2]);
+
+        // In the future, we can create a SourceControlDecorator that adds
+        // a special marker to the icon if artefacts are not committed to the repository.
 
         for (var artefact : artefacts)
             System.out.println(artefact.render());

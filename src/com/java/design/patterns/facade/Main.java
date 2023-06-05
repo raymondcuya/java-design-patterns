df794bb603ec92351597cda7372cbc5193a87d6e
raymond cuya <raymondcuya@gmail.com>
2023-06-05 19:09:29 +1000

    only instantiate the service

--- src/com/java/design/patterns/facade/Main.java
+++ src/com/java/design/patterns/facade/Main.java
@@ -2,11 +2,7 @@ package com.java.design.patterns.facade;
 
 public class Main {
     public static void main(String[] args) {
-        var server = new NotificationServer();
-        var connection = server.connect("ip");
-        var authToken = server.authenticate("appId", "key");
-        var message = new Message("hello world");
-        server.send(authToken, message, "target");
-        connection.disconnect();
+        var service = new NotificationService();
+        service.send("Hello world", "target");
     }
 }

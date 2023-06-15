e0117f5a7ae1b17c9557b1610ca264a6f769443b
raymond cuya <raymondcuya@gmail.com>
2023-06-15 21:42:38 +1000

    solution for the facade exercise

--- src/com/java/design/patterns/facade/exercise/Main.java
+++ src/com/java/design/patterns/facade/exercise/Main.java
@@ -2,11 +2,7 @@ package com.java.design.patterns.facade.exercise;
 
 public class Main {
     public static void main(String[] args) {
-        var oauth = new OAuth();
-        var requestToken = oauth.requestToken("appKey", "secret");
-        var accessToken = oauth.getAccessToken(requestToken);
-
-        var twitterClient = new TwitterClient();
-        var tweets = twitterClient.getRecentTweets(accessToken);
+        var twitterAPI = new TwitterAPI("appKey", "secret");
+        var tweets = twitterAPI.getRecentTweets();
     }
 }

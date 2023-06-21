7a85f8179d3362da88f1248c814f50d77167b4f8
raymond cuya <raymondcuya@gmail.com>
2023-06-21 23:23:38 +1000

    flyweight pattern, moving type and icon to a different class

---
+++ src/com/java/design/patterns/flyweight/PointIcon.java
@@ -0,0 +1,14 @@
+package com.java.design.patterns.flyweight;
+
+public class PointIcon {
+    private final PointType type;
+    private final byte[] icon;
+
+    public PointIcon(PointType type, byte[] icon) {
+        this.type = type;
+        this.icon = icon;
+    }
+    public PointType getType() {
+        return type;
+    }
+}

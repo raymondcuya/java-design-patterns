5747e8be9b724916c81c90eabd2bbfc2aa6845d1
raymond cuya <raymondcuya@gmail.com>
2023-06-26 20:42:13 +1000

    retrieve point icon

--- src/com/java/design/patterns/flyweight/PointService.java
+++ src/com/java/design/patterns/flyweight/PointService.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PointService {
+    public PointService(PointIconFactory iconFactory) {
+        this.iconFactory = iconFactory;
+    }
+
+    private PointIconFactory iconFactory;
     public List<Point> getPoints() {
         List<Point> points = new ArrayList<>();
-        var point = new Point(1,2, PointType.CAFE, null);
+        var point = new Point(1,2, iconFactory.getPointIcon(PointType.CAFE));
         points.add(point);
 
         return points;

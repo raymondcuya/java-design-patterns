a9b64a050605c5802ccecdc602cf0eb668ad4065
raymond cuya <raymondcuya@gmail.com>
2023-07-07 21:48:58 +1000

    calling the factory and flyweight

--- src/com/java/design/patterns/flyweight/exercise/SpreadSheet.java
+++ src/com/java/design/patterns/flyweight/exercise/SpreadSheet.java
@@ -3,16 +3,12 @@ package com.java.design.patterns.flyweight.exercise;
 public class SpreadSheet {
     private final int MAX_ROWS = 3;
     private final int MAX_COLS = 3;
-
-    // In a real app, these values should not be hardcoded here.
-    // They should be read from a configuration file.
-    private final String fontFamily = "Times New Roman";
-    private final int fontSize = 12;
-    private final boolean isBold = false;
-
     private Cell[][] cells = new Cell[MAX_ROWS][MAX_COLS];
+    private CellContextFactory contextFactory;
 
-    public SpreadSheet() {
+    public SpreadSheet(CellContextFactory contextFactory) {
+        this.contextFactory = contextFactory;
+
         generateCells();
     }
 
@@ -22,11 +18,15 @@ public class SpreadSheet {
         cells[row][col].setContent(content);
     }
 
+    // We could have a similar method font setting the fontSize or isBold
+    // for a given cell. I've omitted that here.
     public void setFontFamily(int row, int col, String fontFamily) {
         ensureCellExists(row, col);
 
         var cell = cells[row][col];
-        cells[row][col].setFontFamily(fontFamily);
+        var currentContext = cell.getContext();
+        var context = contextFactory.getContext(fontFamily, currentContext.getFontSize(), currentContext.isBold());
+        cells[row][col].setContext(context);
     }
 
     private void ensureCellExists(int row, int col) {
@@ -39,11 +39,14 @@ public class SpreadSheet {
 
     private void generateCells() {
         for (var row = 0; row < MAX_ROWS; row++)
-            for (var col = 0; col < MAX_COLS; col++) {
-                var cell = new Cell(row, col);
-                cell.setFontFamily(fontFamily);
-                cells[row][col] = cell;
-            }
+            for (var col = 0; col < MAX_COLS; col++)
+                cells[row][col] = new Cell(row, col, getDefaultContext());
+    }
+
+    private CellContext getDefaultContext() {
+        // In a real app, these values should not be hardcoded here.
+        // They should be read from a configuration file.
+        return new CellContext("Times New Roman", 12, false);
     }
 
     public void render() {

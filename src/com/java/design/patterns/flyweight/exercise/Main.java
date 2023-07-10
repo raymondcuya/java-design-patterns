35116c045405b7b3fa3d4bd0a5384ac64033eabb
raymond cuya <raymondcuya@gmail.com>
2023-07-10 21:46:04 +1000

    sample call for the exercise

--- src/com/java/design/patterns/flyweight/exercise/Main.java
+++ src/com/java/design/patterns/flyweight/exercise/Main.java
@@ -2,7 +2,8 @@ package com.java.design.patterns.flyweight.exercise;
 
 public class Main {
     public static void main(String[] args) {
-        var sheet = new SpreadSheet();
+        var contextFactory = new CellContextFactory();
+        var sheet = new SpreadSheet(contextFactory);
         sheet.setContent(0, 0, "Hello");
         sheet.setContent(1, 0, "World");
         sheet.setFontFamily(0, 0, "Arial");

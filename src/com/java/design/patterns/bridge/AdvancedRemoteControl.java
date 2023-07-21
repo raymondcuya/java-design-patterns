abe960ac6150d1ece296810daf6b0f5cd2592076
raymond cuya <raymondcuya@gmail.com>
2023-07-21 23:55:46 +1000

    extends rc

--- src/com/java/design/patterns/bridge/AdvancedRemoteControl.java
+++ src/com/java/design/patterns/bridge/AdvancedRemoteControl.java
@@ -1,5 +1,10 @@
 package com.java.design.patterns.bridge;
 
-public abstract class AdvancedRemoteControl extends RemoteControl {
-    public abstract void setChannel(int number);
+public class AdvancedRemoteControl extends RemoteControl {
+    public AdvancedRemoteControl(Device device) {
+        super(device);
+    }
+    public void setChannel(int number) {
+        device.setChannel(number);
+    }
 }

a43e3fe8118ee8fd00c0b7e779962aa4124e3556
raymond cuya <raymondcuya@gmail.com>
2023-07-25 22:55:04 +1000

    big file

---
+++ src/com/java/design/patterns/proxy/Ebook.java
@@ -0,0 +1,18 @@
+package com.java.design.patterns.proxy;
+
+public class Ebook {
+    private String fileName;
+    public Ebook(String fileName) {
+        this.fileName = fileName;
+        load();
+    }
+    private void load() {
+        System.out.println("Loading the ebook " + fileName);
+    }
+    public void show() {
+        System.out.println("Showing the ebook " + fileName);
+    }
+    public String getFileName() {
+        return fileName;
+    }
+}

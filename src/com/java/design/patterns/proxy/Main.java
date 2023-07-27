9df07e53c739807f852cb7b3fd04ddb17432580b
raymond cuya <raymondcuya@gmail.com>
2023-07-27 23:18:58 +1000

    a call for library with ebooks

---
+++ src/com/java/design/patterns/proxy/Main.java
@@ -0,0 +1,13 @@
+package com.java.design.patterns.proxy;
+
+public class Main {
+    public static void main(String[] args) {
+        var library = new Library();
+        String[] fileNames = {"a", "b", "c"};
+
+        for (var fileName: fileNames)
+            library.add(new Ebook(fileName));
+
+        library.openEbook("a");
+    }
+}
